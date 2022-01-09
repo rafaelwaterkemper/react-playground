040c3971074a3560e2ea201f1012f476795e5fd7
Rafael Marangoni Waterkemper <rafael.waterkemper@betha.com.br>
2022-01-09 08:47:47 -0300

    Added typescript; Using the useEffect to load de data from api once the component is created;

--- fundamentals-reactjs/webpack.config.js
+++ fundamentals-reactjs/webpack.config.js
@@ -6,13 +6,13 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
     devServer: {
         static: path.resolve(__dirname, 'public'),
@@ -22,7 +22,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
